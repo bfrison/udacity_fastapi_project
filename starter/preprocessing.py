cef99ea92ac0870f350ff438fa7a8826e05885c6
Blaise Frison <blaise.frison@gmail.com>
2024-07-03 18:49:42 -0400

    complete preprocessing.py

---
+++ starter/preprocessing.py
@@ -0,0 +1,19 @@
+'''
+This script deduplicates rows from census.csv and stores the results as clean_census.csv
+'''
+
+import os
+
+import pandas as pd
+
+
+if __name__ == '__main__':
+    data_dir = 'data'
+    df = pd.read_csv(
+        os.path.join(data_dir, 'census.csv'),
+        sep=', ',
+        engine='python',
+        na_values='?',
+    ).convert_dtypes()
+    df_dedup = df.drop_duplicates()
+    df_dedup.to_csv(os.path.join(data_dir, 'clean_census.csv'), index=False)
