4fd9f75b252f12e29eba3fd2c831208679476bd9
Blaise Frison <blaise.frison@gmail.com>
2024-07-07 12:07:14 -0400

    linting corrections

--- starter/conftest.py
+++ starter/conftest.py
@@ -1,29 +1,31 @@
-import pandas as pd
 import pytest
-from preprocessing import preprocessing
-from sklearn.linear_model import LogisticRegression
-from sklearn.pipeline import Pipeline
 import yaml
 
-from starter.ml.model import compute_model_metrics, create_pipeline, inference, score_strata, train_model
+from preprocessing import preprocessing
+from starter.ml.model import create_pipeline, train_model
 
 scores = ['f1_score', 'precision_score', 'recall_score']
+
 
 @pytest.fixture
 def data_dir():
     return 'data'
 
+
 @pytest.fixture
 def input_file():
     return 'census.csv'
+
 
 @pytest.fixture
 def df_clean(data_dir, input_file):
     return preprocessing(data_dir, input_file).head(10)
 
+
 @pytest.fixture
 def salary(df_clean):
     return df_clean.salary.head(10)
+
 
 @pytest.fixture
 def parameters():
@@ -31,6 +33,7 @@ def parameters():
         params = yaml.safe_load(f)
 
     return params
+
 
 @pytest.fixture
 def pipeline(parameters):
@@ -43,6 +46,7 @@ def pipeline(parameters):
     pipeline = create_pipeline(cat_cols, num_cols, C, penalty)
 
     return pipeline
+
 
 @pytest.fixture
 def trained_pipeline(pipeline, df_clean, salary):
--- starter/test_model.py
+++ starter/test_model.py
@@ -9,24 +9,40 @@ from starter.ml.model import compute_model_metrics, inference, score_strata
 
 def test_preprocessing_dataframe(df_clean):
 
-    assert isinstance(df_clean, pd.DataFrame), 'Data is not in the Pandas DataFrame format'
+    assert isinstance(
+        df_clean, pd.DataFrame
+    ), 'Data is not in the Pandas DataFrame format'
     assert len(df_clean) > 0, 'Data contains now rows'
-    assert 'salary' in df_clean, 'Data does not contain the \'salary\' target column'
+    assert (
+        'salary' in df_clean
+    ), 'Data does not contain the \'salary\' target column'
+
 
 def test_preprocessing_deduplication(df_clean):
 
     num_duplicates = df_clean.duplicated().sum()
 
-    assert num_duplicates == 0, f'There are {num_duplicates:,d} duplicates in data'
+    assert (
+        num_duplicates == 0
+    ), f'There are {num_duplicates:,d} duplicates in data'
+
 
 def test_create_pipeline(pipeline):
 
-    assert isinstance(pipeline, Pipeline), 'Model is not an instance of Sci-Kit Learn Pipeline'
-    assert isinstance(pipeline[-1], LogisticRegression), 'Model\'s regressor is not an instance of LogisticRegressor'
+    assert isinstance(
+        pipeline, Pipeline
+    ), 'Model is not an instance of Sci-Kit Learn Pipeline'
+    assert isinstance(
+        pipeline[-1], LogisticRegression
+    ), 'Model\'s regressor is not an instance of LogisticRegressor'
+
 
 def test_train(trained_pipeline):
 
-    assert trained_pipeline.__sklearn_is_fitted__(), 'Model is not fitted after training'
+    assert (
+        trained_pipeline.__sklearn_is_fitted__()
+    ), 'Model is not fitted after training'
+
 
 @pytest.mark.parametrize('score', scores)
 def test_compute_model_metrics(trained_pipeline, df_clean, salary, score):
@@ -36,19 +52,30 @@ def test_compute_model_metrics(trained_pipeline, df_clean, salary, score):
     assert f'{score}' in score_vals, f'{score} not in score values'
     assert isinstance(score_vals[f'{score}'], float), f'{score} is not a float'
 
+
 def test_inference(trained_pipeline, df_clean, salary):
 
     y_pred = inference(trained_pipeline, df_clean)
 
-    assert isinstance(y_pred, pd.Series), 'Inferred data is not in the Pandas Series format'
-    assert set(y_pred) == set(salary), 'The set of inferred values does not correspond to original target data'
+    assert isinstance(
+        y_pred, pd.Series
+    ), 'Inferred data is not in the Pandas Series format'
+    assert set(y_pred) == set(
+        salary
+    ), 'The set of inferred values does not correspond to original target data'
+
 
 def test_score_strata_vals(trained_pipeline, df_clean, salary):
 
     stratum_dict = score_strata(trained_pipeline, df_clean, salary, 'sex')
 
-    assert isinstance(stratum_dict, dict), 'Scores are not in the Pandas Series Format'
-    assert set(df_clean['sex']) == set(stratum_dict.keys()), 'Values in stratum do not correspond to values in original data'
+    assert isinstance(
+        stratum_dict, dict
+    ), 'Scores are not in the Pandas Series Format'
+    assert set(df_clean['sex']) == set(
+        stratum_dict.keys()
+    ), 'Values in stratum do not correspond to values in original data'
+
 
 @pytest.mark.parametrize('score', scores)
 def test_score_strata_scores(trained_pipeline, df_clean, salary, score):
