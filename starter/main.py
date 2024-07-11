27f0560ca4614f913b84bad175862ecaba3ead34
Blaise Frison <blaise.frison@gmail.com>
2024-07-10 23:30:07 -0400

    allow start of web app without model

--- starter/main.py
+++ starter/main.py
@@ -9,6 +9,7 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 import pandas as pd
 from pydantic import BaseModel, Field
+from sklearn.pipeline import Pipeline
 
 from starter.ml.model import inference
 
@@ -33,10 +34,26 @@ class CensusEntry(BaseModel):
     native_country: str | None = Field(alias='native-country')
 
 
+model_path = os.path.join('model', 'logistic_regression.pkl')
+model = None
+
 app = FastAPI()
 
-with open(os.path.join('model', 'logistic_regression.pkl'), 'rb') as f:
-    model = pickle.load(f)
+
+def load_model() -> Pipeline | None:
+    '''
+    This function checks if the model is already loaded, and loads it if it is
+    not the case
+    '''
+    global model
+    if not model:
+        if os.path.exists(model_path):
+            with open(model_path, 'rb') as f:
+                model = pickle.load(f)
+        else:
+            model = None
+
+    return model
 
 
 @app.get('/')
@@ -47,12 +64,28 @@ async def greet() -> str:
     return PlainTextResponse('Welcome to the salary predictor!')
 
 
+@app.get('/check_model')
+async def check_model() -> str:
+    '''
+    This get method checks if the model is correctly loaded
+    '''
+    model = load_model()
+    if model:
+        return PlainTextResponse('Model is properly loaded')
+    else:
+        return PlainTextResponse('Model is not loaded yet')
+
+
 @app.post('/infer/')
 async def infer_salary(entries: list[CensusEntry]) -> list[str]:
     '''
     This post method accepts a DataFrame in json format (orient='records') and
     returns a list of salary predictions
     '''
+    global model
+    if not model:
+        model = load_model()
+
     df = pd.DataFrame([entry.model_dump(by_alias=True) for entry in entries])
 
     y_pred = inference(model, df)
